8f4d0b6978a22b166a68880a3556315b79669cde
Yuval Schreiber <yuvaliko597@gmail.com>
2026-05-17 18:57:56 +0300

    Add SonarQube project creation and local scan functions

--- vars/codeQuality.groovy
+++ vars/codeQuality.groovy
@@ -46,3 +46,25 @@ def sonarLocalScanWithGate() {
         }
     }
 }
+def sonarCreateProject(String projectKey) {
+        withSonarQubeEnv('SonarQubeScanner') {
+            sh """
+                curl -s -u ${env.SONAR_TOKEN}: \
+               -X POST "${env.SONAR_HOST_URL}/api/projects/create" \
+               -d "project=${projectKey}&name=${projectKey}"
+            """
+        }
+}
+
+def sonarLocalScan() {
+    def scannerHome = tool 'SonarQubeScanner'
+    withSonarQubeEnv('SonarQubeScanner') {
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=${env.JOB_NAME} \
+            -Dsonar.projectName=${env.JOB_NAME} \
+            -Dsonar.sources=. \
+            -Dsonar.sourceEncoding=UTF-8
+        """
+    }
+}
